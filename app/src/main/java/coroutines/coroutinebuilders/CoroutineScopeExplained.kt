7b6a7370f081de3bb49d2ed2111d563d410e363f
stefan bayne <stefanbayne@gmail.com>
2024-12-30 14:56:00 -0500

    chore: comments

--- app/src/main/java/coroutines/coroutinebuilders/CoroutineScopeExplained.kt
+++ app/src/main/java/coroutines/coroutinebuilders/CoroutineScopeExplained.kt
@@ -3,4 +3,9 @@ package coroutines.coroutinebuilders
 /*
 When we want to use a coroutine inside a suspending function, we use CoroutineScope.
 Coroutine Scopes allow us also to cancel multiple coroutines at the same time.
+
+A CoroutineScope is just an interface that holds a reference to a CoroutineContext.
+A CoroutineContext is a context that holds all the meta data about the current
+coroutine.
+Data like name, job, dispatcher, etc.
  */
