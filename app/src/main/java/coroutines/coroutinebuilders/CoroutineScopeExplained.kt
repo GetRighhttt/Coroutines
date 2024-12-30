f725aa8ef566c68fc9065605c15cea346a8ba2cf
stefan bayne <stefanbayne@gmail.com>
2024-12-30 15:04:30 -0500

    feat: adding coroutine scope info

--- app/src/main/java/coroutines/coroutinebuilders/CoroutineScopeExplained.kt
+++ app/src/main/java/coroutines/coroutinebuilders/CoroutineScopeExplained.kt
@@ -1,5 +1,15 @@
 package coroutines.coroutinebuilders
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
+
 /*
 When we want to use a coroutine inside a suspending function, we use CoroutineScope.
 Coroutine Scopes allow us also to cancel multiple coroutines at the same time.
@@ -9,3 +19,21 @@ A CoroutineContext is a context that holds all the meta data about the current
 coroutine.
 Data like name, job, dispatcher, etc.
  */
+
+fun main() {
+
+    runBlocking {
+
+        val handler = CoroutineExceptionHandler
+        val context = coroutineContext[Job]
+        val coroutineContextEx = CoroutineScope(
+            Dispatchers.Main
+                    + CoroutineName("first_coroutine")
+                    + Job()
+        )
+        print(coroutineContextEx)
+    }
+    /*
+    CoroutineScope(coroutineContext=[CoroutineName(first_coroutine), JobImpl{Active}@17ed40e0, Dispatchers.Main[missing, cause=java.lang.RuntimeException: Stub!]])
+     */
+}
