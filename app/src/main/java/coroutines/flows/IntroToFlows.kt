2088f07bf7522eb517b945c2dd46eebc6e5d51b6
stefan bayne <stefanbayne@gmail.com>
2024-12-30 22:47:10 -0500

    chore: updating flow content

--- app/src/main/java/coroutines/flows/IntroToFlows.kt
+++ app/src/main/java/coroutines/flows/IntroToFlows.kt
@@ -4,17 +4,19 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.lang.RuntimeException
 
-/*
-Flows are streams of data that can be computed asynchronously.
-"A cold, asynchronous stream and is an implementation of the Observable design pattern."
-Cold = starts when user subscribes to it, starts anew for each new subscriber.
-Hot = continuously emits results.
-
-Observable design pattern has two methods - subscribe() and publish() btw.
-With Flows emit() = subscribe() and collect() = publish().
-Produces(data sources/repositories) emit data to the flow, that can then be collected(ui layer) by the UI.
-
-Flows are suspending but not concurrent, are cold, and use backpressure: resisting flow of data.
+/**
+ * Flows are streams of data that can be computed asynchronously.
+ * "A cold, asynchronous stream and is an implementation of the Observable design pattern."
+ * Cold = starts when user subscribes to it, starts anew for each new subscriber.
+ * Hot = continuously emits results.
+ *
+ * Observable design pattern has two methods - subscribe() and publish() btw.
+ * With Flows emit() = subscribe() and collect() = publish().
+ * Producers(data sources/repositories) emit data to the flow, that can then be collected(UI layer) by the UI.
+ *
+ * Flows are suspending but not concurrent, are cold, and use backpressure: resisting flow of data.
+ *
+ * Flow = emit/collect (sub/pub) and are cold.
  */
 
 // must be marked as suspend to say it is a coroutine function
